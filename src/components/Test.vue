1e667e58adb597bc92c682bc645b8fb4da4885b9
Thatchakon Jom-ud <57070053@kmitl.ac.th>
2021-02-21 00:40:35 +0900

    trying to use setup and ref hooks

---
+++ src/components/Test.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    {{ msg }}
+    <input v-model="inputValue" />
+    <button
+      @click="changingMsg(inputValue)"
+    >
+      Change Msg
+    </button>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref, Ref } from 'vue'
+export default defineComponent({
+  name: 'Hello',
+  setup () {
+    const msg: Ref<String> = ref('###Test###')
+    const inputValue: Ref<String> = ref('')
+    const changingMsg : Function = (val: String) => {
+      msg.value = val
+    }
+    return {
+      msg,
+      inputValue,
+      changingMsg
+    }
+  }
+})
+</script>
+
+<style>
+</style>
